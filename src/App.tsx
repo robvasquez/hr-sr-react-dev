ff9594fa99796b542c925b92cb67644463109349
Rob Vasquez <rvasquez@18techs.com>
2023-10-25 21:24:09 -0600

    removing solutions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import TodoList from "./components/todoList/TodoList";
 
 function App() {
   return (
-    <TodoList />
+    <>
+      <p>ToDos Test</p>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-import store from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}> {/* Wrap your app with the Provider and pass in the store */}
+={/* Wrap your app with the Provider and pass in the store */}
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
